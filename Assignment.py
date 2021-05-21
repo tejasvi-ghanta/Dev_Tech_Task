58b23d353c91c46ba66641bf02e1b5abe04b99f6
Ramakrishna Moturi <ramakrishnamoturi@Ramakrishnas-MacBook-Pro.local>
2021-05-21 11:42:51 +0100

    clean up

--- Assignment.py
+++ Assignment.py
@@ -1,20 +1,25 @@
 import gzip
 import pandas as pd
 
+file_locations = "/Users/ramakrishnamoturi/Downloads/name.basics.tsv.gz"
+key_word_to_search = "producer"
+
 
 def open_file(file_location):
     unzip_file = gzip.open(file_location, 'rb')
     return pd.read_csv(unzip_file, sep='\t')
 
 
-def print_count(file_read):
+def print_count(file_read, key_word):
     convert_df_tp_list = file_read.values.tolist()
     count = 0
     for row in convert_df_tp_list:
-        if row[3] == "\\N" and str(row[4]).__contains__("producer"):
+        if row[3] == "\\N" and str(row[4]).__contains__(key_word):
             count = count + 1
-    print("The number of people who are alive & producers are  ", count)
+    print("The number of people who are alive & producers are ", count)
 
 
-file = open_file("/Users/ramakrishnamoturi/Downloads/name.basics.tsv.gz")
-print_count(file)
+file = open_file(file_locations)
+# Change the file location where the file is
+print_count(file, key_word_to_search)
+# We can change the primary keyword to anything.
